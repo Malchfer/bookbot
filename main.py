46d62a7562212859d25c8892a85f60d4ae8aca7f
Malchfer <chrippa.m@gmail.com>
2025-11-22 18:51:55 +0100

    created get_book_text and defined main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(book):
+        with open(book) as f:
+            read_data = f.read()
+            return read_data        
+        
+        
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+main()
