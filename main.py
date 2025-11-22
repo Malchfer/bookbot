21c964e8cbba2168c34d9ab8bfd90d6a9fa2df25
Malchfer <chrippa.m@gmail.com>
2025-11-22 21:44:30 +0100

    added report output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words,count_characters
+from stats import count_words,count_characters, dict_sort
 
 def get_book_text(input):
     with open(input) as f:
@@ -7,7 +7,14 @@ def get_book_text(input):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {count_words(book_text)} total words")
-    print(count_characters(book_text))
+    print("--------- Character Count -------")
+    for character in dict_sort(count_characters(book_text)):
+        if character["char"].isalpha():
+            print(f"{character["char"]}: {character["num"]}")
+    print("============= END ===============")
 
 main()
