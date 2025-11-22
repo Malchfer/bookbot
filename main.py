e182e0d32d51ab214e83b6a0fd97fc634e62828a
Malchfer <chrippa.m@gmail.com>
2025-11-22 19:20:21 +0100

    create count_words and add stats.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-def get_book_text(book):
-        with open(book) as f:
+def get_book_text(input):
+        with open(input) as f:
             read_data = f.read()
             return read_data        
+
+def count_words(input):
+        word_count = len(input.split())
+        return word_count
         
-        
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    print(f"Found {count_words(book_text)} total words")
 
 main()
